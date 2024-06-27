eebe12c0e45b2668acc8b2aa84ee9dd08b3030a6
karatas <cem.karatas@outlook.de>
2024-06-27 17:42:03 +0200

    github actions

---
+++ src/test/java/com/polarbookshop/catalogservice/BookControllerMvcTest.java
@@ -0,0 +1,31 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest; import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BookControllerMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception{
+        String isbn = "234241342";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/"+ isbn))
+                .andExpect(status().isNotFound());
+    }
+}
